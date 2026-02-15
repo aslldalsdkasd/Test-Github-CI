5c9d5164c9e6e2cdb5db0f7fb285fca7c1be2486
aslldalsdkasd <122563056+aslldalsdkasd@users.noreply.github.com>
2026-02-15 19:47:26 +0300

    test-1

--- module_26_fastapi/homework/app_fast.py
+++ module_26_fastapi/homework/app_fast.py
@@ -1,12 +1,16 @@
 from typing import List, Optional
-#d
+
+
 from fastapi import FastAPI
-from schema import AllRecipes, DetailsRecipes
 from sqlalchemy.future import select
-from database import engine, session
-import schema
+
 import model
+import schema
+from database import engine, session
+from schema import AllRecipes, DetailsRecipes
+
 app = FastAPI()
+
 
 @app.on_event("startup")
 async def startup():
@@ -14,16 +18,19 @@ async def startup():
     async with engine.begin() as conn:
         await conn.run_sync(model.Base.metadata.create_all)
 
+
 @app.on_event("shutdown")
 async def shutdown():
     """Действия при завершении работы"""
     await engine.dispose()
     await session.close()
 
+
 @app.get("/")
 async def root():
     """Приветственная страница"""
     return {"message": "Hello World"}
+
 
 @app.post("/recipes", response_model=schema.Recipe)
 async def create_recipe(recipe: schema.Recipe) -> model.Recipe:
@@ -33,21 +40,24 @@ async def create_recipe(recipe: schema.Recipe) -> model.Recipe:
         session.add(new_recipe)
     return new_recipe
 
-@app.get('/recipes', response_model=List[schema.AllRecipes])
+
+@app.get("/recipes", response_model=List[schema.AllRecipes])
 async def get_recipes() -> List[schema.AllRecipes]:
     """Эндпоинт для вывода всех рецептов"""
     res = await session.execute(
-        select(model.Recipe)
-        .order_by(model.Recipe.vies.desc(), model.Recipe.time_min))
+        select(model.Recipe).order_by(model.Recipe.vies.desc(), model.Recipe.time_min)
+    )
     return res.scalars().all()
 
-@app.get('/recipes/{recipe_id}', response_model=schema.DetailsRecipes)
+
+@app.get("/recipes/{recipe_id}", response_model=schema.DetailsRecipes)
 async def get_recipe(recipe_id: int) -> schema.DetailsRecipes:
     """Эндпоинт для вывода определенного рецепта"""
-    res = await session.execute(select(model.Recipe).filter(model.Recipe.id == recipe_id))
+    res = await session.execute(
+        select(model.Recipe).filter(model.Recipe.id == recipe_id)
+    )
     recipe = res.scalars().first()
     if recipe:
         recipe.vies += 1
         await session.commit()
     return recipe
-
